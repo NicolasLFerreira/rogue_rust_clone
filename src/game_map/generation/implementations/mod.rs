e75c2712902cbb22a7b70cfa5312a23ce9c29c9c
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-09-02 21:41:18 +1200

    renamed and restructured

---
+++ src/game_map/generation/implementations/mod.rs
@@ -0,0 +1,3 @@
+pub mod prototype_map_generator;
+mod dungeon_map_generator;
+mod utils;
---
+++ src/game_map/generation/implementations/utils.rs
@@ -0,0 +1,39 @@
+use crate::game_map::tile_map::TileMap;
+use crate::geometry::point::Point;
+use crate::geometry::rect::Rect;
+use rand::Rng;
+
+// Contextual representation of a region inside a tile map
+#[derive(Copy, Clone)]
+pub(super) struct Region {
+    pub rect: Rect,
+}
+
+// Picks a point in one of the walls
+pub(super) fn pick_wall_point(rect: Rect) -> Point {
+    match rand::rng().random_range(0..4) {
+        0 => Point::new(
+            rect.x,
+            rand::rng().random_range(rect.y..rect.y + rect.height),
+        ),
+        1 => Point::new(
+            rect.x + rect.width - 1,
+            rand::rng().random_range(rect.y..rect.y + rect.height),
+        ),
+        2 => Point::new(
+            rand::rng().random_range(rect.x..rect.x + rect.width),
+            rect.y,
+        ),
+        3 => Point::new(
+            rand::rng().random_range(rect.x..rect.x + rect.width),
+            rect.y + rect.height - 1,
+        ),
+        _ => Point::ZERO,
+    }
+}
+
+pub(super) fn apply_tile_map(host: &mut TileMap, other: &TileMap) {
+    for (point, tile) in other.iter_tiles() {
+        host.set(point, tile.clone());
+    }
+}
