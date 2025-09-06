71b1afa85f3f90f9f5ed045e915ffa8c2f1dd0a5
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-09-06 21:32:40 +1200

    reverted main loop

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,7 @@ use crate::graphics::graphics::Graphics;
 use crate::graphics::rendering::renderers::crossterm_renderer::CrosstermRenderer;
 use crate::graphics::theme::AsciiTheme;
 use crate::graphics::window::implementations::crossterm_window::CrosstermGameWindow;
-use crate::graphics::window::implementations::winit_window::WinitGameWindow;
 use crate::state::State;
-use winit::event_loop::{ControlFlow, EventLoop};
 
 const SCREEN_WIDTH: usize = 80;
 const SCREEN_HEIGHT: usize = 25;
@@ -24,33 +22,15 @@ const SCREEN_HEIGHT: usize = 25;
 fn main() -> std::io::Result<()> {
     // Screen dimensions
     let window_size: Rect = Rect::new_dimensions(SCREEN_WIDTH, SCREEN_HEIGHT);
-    let toggle = true;
 
-    // winit
-    if toggle {
-        let graphics = Graphics::new(
-            Box::new(AsciiTheme),
-            Box::new(CrosstermRenderer::new(window_size)),
-        );
-        let window = WinitGameWindow::default();
-        let state = State::new(window_size.resize(0, -3));
+    // Initialise engine (utilising crossterm at the moment)
+    let graphics = Graphics::new(
+        Box::new(AsciiTheme),
+        Box::new(CrosstermRenderer::new(window_size)),
+    );
+    let window = CrosstermGameWindow::new(window_size);
+    let state = State::new(window_size.resize(0, -3));
 
-        let mut engine = Engine::<WinitGameWindow>::new(graphics, window, state);
-        engine.run()?;
-
-        Ok(())
-    }
-    // Crossterm
-    else {
-        // Initialise engine (utilising crossterm at the moment)
-        let graphics = Graphics::new(
-            Box::new(AsciiTheme),
-            Box::new(CrosstermRenderer::new(window_size)),
-        );
-        let window = CrosstermGameWindow::new(window_size);
-        let state = State::new(window_size.resize(0, -3));
-
-        let mut engine = Engine::<CrosstermGameWindow>::new(graphics, window, state);
-        engine.run()
-    }
+    let mut engine = Engine::<CrosstermGameWindow>::new(graphics, window, state);
+    engine.run()
 }
